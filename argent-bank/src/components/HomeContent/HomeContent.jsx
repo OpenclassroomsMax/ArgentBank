c8738658efe29c21ebfa635f7f3abf7c3456fa3a
Jezequel_Maxence <maxence.jezequel@hotmail.com>
2024-02-19 16:23:17 +0100

    Ajout de la page Home

---
+++ argent-bank/src/components/HomeContent/HomeContent.jsx
@@ -0,0 +1,11 @@
+import "../HomeContent/HomeContent.css"
+
+export default function HomeContent({ img, title, description }) {
+  return (
+    <div class="feature-item">
+      <img src={img} alt="Chat Icon" class="feature-icon" />
+      <h3 class="feature-item-title">{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
--- argent-bank/src/pages/Home/Home.jsx
+++ argent-bank/src/pages/Home/Home.jsx
@@ -1,9 +1,45 @@
 
+
+import iconChat from "../../assets/icon-chat.png";
+import iconMoney from "../../assets/icon-money.png";
+import iconSecurity from "../../assets/icon-security.png";
+
+import HomeContent from "../../components/HomeContent/HomeContent";
+
+import "../Home/Home.css"
+
 export default function Home() {
   return (
     <main>
-      <p>no fees.</p>
-
+      <div class="hero">
+        <section class="hero-content">
+          <h2 class="sr-only">Promoted Content</h2>
+          <p class="subtitle">No fees.</p>
+          <p class="subtitle">No minimum deposit.</p>
+          <p class="subtitle">High interest rates.</p>
+          <p class="text">Open a savings account with Argent Bank today!</p>
+        </section>
+      </div>
+      <section class="features">
+        <h2 class="sr-only">Features</h2>
+        <HomeContent
+          img={iconChat}
+          title="You are our #1 priority"
+          description="Need to talk to a representative? You can get in touch through our
+            24/7 chat or through a phone call in less than 5 minutes."
+        />
+        <HomeContent
+          img={iconMoney}
+          title="More savings means higher rates"
+          description="The more you save with us, the higher your interest rate will be!"
+        />
+        <HomeContent
+          img={iconSecurity}
+          title="Security you can trust"
+          description=" We use top of the line encryption to make sure your data and money
+            is always safe."
+        />
+      </section>
     </main>
   );
 }
